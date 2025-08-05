1aa6e060a8addbc23b3770e2da602988c1d1626f
Sander Sekreve <sander.sekreve@hva.nl>
2025-08-05 14:40:38 +0200

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import ExamPage from "./pages/ExamPage";
 import NotFound from "./pages/NotFound";
 import ResultScreen from "./pages/ResultScreen";
 import OverView from "./pages/OverView";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/overzicht" element={<OverView />} />
-        <Route path="/examen/:examId" element={<ExamPage />} />
-        <Route path="/examen/:examId/resultaat" element={<ResultScreen />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/overzicht" element={<OverView />} />
+          <Route path="/examen/:examId" element={<ExamPage />} />
+          <Route path="/examen/:examId/resultaat" element={<ResultScreen />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
